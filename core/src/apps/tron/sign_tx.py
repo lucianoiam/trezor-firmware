34c9c3e3ecbb88c41aef2f62aad7bc18253a9087
PrisionMike <su.sh2396@gmail.com>
2026-01-05 10:32:06 +0100

    feat(tron): Unblind Shasta USDT transfer
    - Hardcoded USDT address
    - Contract parsing logic identical to Ethereum
    - Imports from app/ethereum.
    - Some TODOs added for subsequent feats.
    
    [no changelog]

--- core/src/apps/tron/sign_tx.py
+++ core/src/apps/tron/sign_tx.py
@@ -2,15 +2,24 @@ from micropython import const
 from typing import TYPE_CHECKING
 
 from trezor import messages
+from trezor.messages import EthereumTokenInfo
 from trezor.protobuf import dump_message_buffer
 from trezor.wire import DataError
 
 from apps.common.keychain import with_slip44_keychain
 
+from ..ethereum.tokens import UNKNOWN_TOKEN
 from . import CURVE, PATTERN, SLIP44_ID, consts, layout
 
 if TYPE_CHECKING:
-    from trezor.messages import TronRawContract, TronSignature, TronSignTx
+    from buffer_types import AnyBytes
+
+    from trezor.messages import (
+        TronRawContract,
+        TronSignature,
+        TronSignTx,
+        TronTriggerSmartContract,
+    )
     from trezor.protobuf import MessageType
 
     from apps.common.keychain import Keychain
@@ -21,7 +30,7 @@ async def sign_tx(msg: TronSignTx, keychain: Keychain) -> TronSignature:
     from trezor import TR
     from trezor.crypto.curve import secp256k1
     from trezor.crypto.hashlib import sha256
-    from trezor.ui.layouts import confirm_blob, confirm_tron_send, show_continue_in_app
+    from trezor.ui.layouts import confirm_blob, show_continue_in_app
     from trezor.wire.context import call_any
 
     from apps.common import paths
@@ -49,14 +58,7 @@ async def sign_tx(msg: TronSignTx, keychain: Keychain) -> TronSignature:
         raise DataError("Tron: fees too high")
 
     contract = await call_any(messages.TronContractRequest(), *consts.CONTRACT_TYPES)
-    raw_contract, total_send = await process_contract(contract)
-
-    fee_string = (
-        layout.format_energy_amount(fee_limit)
-        if messages.TronTriggerSmartContract.is_type_of(contract)
-        else None
-    )
-    await confirm_tron_send(total_send, fee_string)
+    raw_contract = await process_contract(contract, fee_limit)
 
     raw_tx = messages.TronRawTransaction(
         ref_block_bytes=msg.ref_block_bytes,
@@ -81,22 +83,22 @@ async def sign_tx(msg: TronSignTx, keychain: Keychain) -> TronSignature:
 
 async def process_contract(
     contract: MessageType,
-) -> tuple[TronRawContract, str | None]:
+    fee_limit: int,
+) -> TronRawContract:
     from trezor.enums import TronRawContractType
+    from trezor.ui.layouts import confirm_tron_send
 
     _INT64_MAX = const(9_223_372_036_854_775_807)
 
-    total_send = None
     if messages.TronTransferContract.is_type_of(contract):
         contract_type = TronRawContractType.TransferContract
         await layout.confirm_transfer_contract(contract)
         if contract.amount > _INT64_MAX:
             raise DataError("Tron: invalid transfer amount")
-        total_send = layout.format_trx_amount(contract.amount)
+        await confirm_tron_send(layout.format_trx_amount(contract.amount), None)
     elif messages.TronTriggerSmartContract.is_type_of(contract):
         contract_type = TronRawContractType.TriggerSmartContract
-        await layout.confirm_unkown_smart_contract(contract)
-        # TODO: Extract from contract.data to total_send
+        await process_smart_contract(contract, fee_limit)
     else:
         raise DataError("Tron: contract type unknown")
 
@@ -110,4 +112,67 @@ async def process_contract(
         ),
     )
 
-    return raw_contract, total_send
+    return raw_contract
+
+
+async def process_smart_contract(
+    contract: TronTriggerSmartContract, fee_limit: int
+) -> None:
+    token = get_token_info(contract.contract_address)
+    if token is not UNKNOWN_TOKEN and len(contract.data) == 68:
+        result = await process_known_trc20_contract(contract, token, fee_limit)
+        if result:
+            return None
+    await layout.confirm_unkown_smart_contract(contract, fee_limit)
+
+
+# TODO: Maybe refactor with ETH. Code duplicated from ethereum/sign_tx.py:_handle_known_contract_calls
+async def process_known_trc20_contract(
+    contract: TronTriggerSmartContract, token: EthereumTokenInfo, fee_limit: int
+) -> None | bool:
+    from trezor.utils import BufferReader
+
+    from ..ethereum.sc_constants import (
+        SC_ARGUMENT_ADDRESS_BYTES,
+        SC_ARGUMENT_BYTES,
+        SC_FUNC_SIG_BYTES,
+        SC_FUNC_SIG_TRANSFER,
+    )
+
+    data_reader = BufferReader(contract.data)
+    func_sig = data_reader.read_memoryview(SC_FUNC_SIG_BYTES)
+    if func_sig == SC_FUNC_SIG_TRANSFER:
+        if data_reader.remaining_count() < SC_ARGUMENT_BYTES * 2:
+            return None
+        arg0 = data_reader.read_memoryview(SC_ARGUMENT_BYTES)
+        assert all(
+            byte == 0 for byte in arg0[: SC_ARGUMENT_BYTES - SC_ARGUMENT_ADDRESS_BYTES]
+        )
+        recipient = b"\x41" + bytes(
+            arg0[SC_ARGUMENT_BYTES - SC_ARGUMENT_ADDRESS_BYTES :]
+        )
+        arg1 = data_reader.read_memoryview(SC_ARGUMENT_BYTES)
+        value = int.from_bytes(arg1, "big")
+    else:
+        return None
+
+    await layout.confirm_known_trc20_smart_contract(recipient, value, fee_limit, token)
+    return True  # Couldn't figure out a better way to indicate success here.
+
+
+# TODO: Place holder for actual logic
+def get_token_info(token_address: AnyBytes) -> EthereumTokenInfo:
+    # Shasta testnet USDT
+    if (
+        token_address
+        == b"\x41\x42\xa1\xe3\x9a\xef\xa4\x92\x90\xf2\xb3\xf9\xed\x68\x8d\x7c\xec\xf8\x6c\xd6\xe0"
+    ):
+        return EthereumTokenInfo(
+            address=b"\x41\x42\xa1\xe3\x9a\xef\xa4\x92\x90\xf2\xb3\xf9\xed\x68\x8d\x7c\xec\xf8\x6c\xd6\xe0",
+            chain_id=195,
+            decimals=6,
+            name="",
+            symbol="tUSDT",
+        )
+    else:
+        return UNKNOWN_TOKEN  # TODO: Wei Not used in TRON.
