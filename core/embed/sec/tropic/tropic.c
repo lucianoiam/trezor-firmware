3743d7a3ae94e3de4fe73a6ed097206638a29cd9
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2026-01-20 11:52:06 +0100

    feat(core/prodtest): introduce logging in `tropic_wait_for_ready()`

--- core/embed/sec/tropic/tropic.c
+++ core/embed/sec/tropic/tropic.c
@@ -98,7 +98,7 @@ static bool is_retryable(lt_ret_t ret) {
         tropic01_reset();                                                   \
         tropic_deinit();                                                    \
         tropic_init();                                                      \
-        tropic_wait_for_ready();                                            \
+        tropic_wait_for_ready(NULL);                                        \
         if (TROPIC_RETRY_COMMAND_session_started) {                         \
           if (tropic_custom_session_start(                                  \
                   NULL, TROPIC_RETRY_COMMAND_pairing_key_index) != LT_OK) { \
@@ -194,10 +194,14 @@ bool tropic_get_cert_chain_ptr(cli_t *cli, uint8_t const **cert_chain,
 }
 #endif  // !PRODUCTION || defined(TREZOR_PRODTEST)
 
-bool tropic_wait_for_ready(void) {
+// `cli` may be `NULL`
+bool tropic_wait_for_ready(cli_t *cli) {
   tropic_driver_t *drv = &g_tropic_driver;
 
   if (!drv->initialized) {
+    if (cli) {
+      cli_trace(cli, "Tropic driver is not initialized");
+    }
     return false;
   }
 
@@ -215,6 +219,9 @@ bool tropic_wait_for_ready(void) {
     }
   }
 
+  if (cli) {
+    cli_trace(cli, "Tropic is busy");
+  }
   return false;
 }
 
@@ -286,7 +293,10 @@ lt_ret_t tropic_custom_session_start(cli_t *cli,
     }
   }
 
-  tropic_wait_for_ready();
+  bool res = tropic_wait_for_ready(cli);
+  if (!res) {
+    goto cleanup;
+  }
 
   ret = TROPIC_RETRY_COMMAND(lt_session_start(&drv->handle, tropic_public,
                                               pairing_key_index, trezor_private,
