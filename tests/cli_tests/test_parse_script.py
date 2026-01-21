efbb5d29d0372c35f7d5ae2446d5cb913f88f508
Luciano Iam <oss@lucianoiam.com>
2026-01-21 10:48:56 +0100

    Use less classes in script parser
    Align with Trezor codebase practices
    
    TODO: formal check (read style guide?)

--- tests/cli_tests/test_parse_script.py
+++ tests/cli_tests/test_parse_script.py
@@ -2,7 +2,7 @@
 import sys
 sys.path.insert(0, "../../core/src/apps/bitcoin")
 
-from parse_script import ScriptParser
+from parse_script import parse_script, tree_repr, get_spending_paths
 
 script = "wsh(or_d(pk(@0/<0;1>/*),and_v(v:pkh(@1/<0;1>/*),older(1))))"
 
@@ -10,14 +10,14 @@ print("--- Script ---")
 print(script)
 
 # Parse once
-root = ScriptParser(script).parse()
+root = parse_script(script)
 
 print("\n--- Script Tree (raw) ---")
-print(root.tree_repr())
+print(tree_repr(root))
 
 print("--- Policy Tree (normalized) ---")
-print(root.tree_repr(normalize=True))
+print(tree_repr(root, normalize=True))
 
 print("--- Spending Paths ---")
-for step in root.get_spending_paths():
+for step in get_spending_paths(root):
     print(step)
